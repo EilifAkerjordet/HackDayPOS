0714325a31e24dc50ca6eb987a0144a359232355
EilifAkerjordet <64464354+EilifAkerjordet@users.noreply.github.com>
2020-07-19 23:45:16 +0200

    heroku setup

--- dbConfig.js
+++ dbConfig.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+//import dotenv from 'dotenv';
 
-dotenv.config();
+//dotenv.config();
 
 const connectDB = () => {
   mongoose.connect(process.env.DATABASE_URL, {
