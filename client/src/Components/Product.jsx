a3f2ed9f7f132e70d64359334415d86fdf353225
EilifAkerjordet <64464354+EilifAkerjordet@users.noreply.github.com>
2020-07-19 17:54:18 +0200

    some cleanup

--- client/src/Components/Product.jsx
+++ client/src/Components/Product.jsx
@@ -7,24 +7,27 @@ import {
 const Product = ({
   name, price, dispatch,
 }) => (
-  <Grid item xs={12} md={6} lg={4}>
-    <Card style={{ backgroundColor: '#646464' }}>
-      <Typography variant="h6">{name}</Typography>
-      <Grid container directon="row" justify="space-between">
-        <Typography paragraph>{`$${price}`}</Typography>
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={() => dispatch({
-            type: 'ADD_TO_TRANSACTION',
-            payload: { name, price, quantity: 1 },
-          })}
-        >
-          Add
-        </Button>
-      </Grid>
-    </Card>
-  </Grid>
+  <>
+    <Grid item xs={12} md={6} lg={4}>
+      <Card style={{ backgroundColor: '#646464' }}>
+        <Typography variant="h6">{name}</Typography>
+        <Grid container directon="row" justify="space-between">
+          <Typography paragraph>{`$${price}`}</Typography>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => dispatch({
+              type: 'ADD_TO_TRANSACTION',
+              payload: { name, price, quantity: 1 },
+            })}
+          >
+            Add
+          </Button>
+        </Grid>
+      </Card>
+    </Grid>
+    <Grid item xs={1} />
+  </>
 
 );
 
